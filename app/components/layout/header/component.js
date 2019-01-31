3f249455160ca7aa72be0f92c518da9d41545769
Roman <rastopyr@gmail.com>
2019-01-31 14:42:53 +0200

    fix: add logout button

--- app/components/layout/header/component.js
+++ app/components/layout/header/component.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember-decorators/service';
 
 export default class LayoutHeaderComponent extends Component {
+  @service session;
+
+  logout() {
+    this.session.invalidate();
+  }
 }
